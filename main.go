ff22680008e23e178b8bf722aed952946e619d89
HilerChen <hilerchyn@gmail.com>
2016-08-16 12:25:14 +0800

    add layout template

--- main.go
+++ main.go
@@ -4,23 +4,33 @@ import(
   "github.com/kataras/iris"
   "github.com/kataras/iris/utils"
   "github.com/iris-contrib/template/markdown"
+  "github.com/iris-contrib/template/html"
   "github.com/iris-contrib/middleware/recovery"
 
   customHeader "github.com/hilerchyn/iris-go.cn/middleware/header"
+
+  "html/template"
 )
 
 func main(){
 
+  iris.UseTemplate(html.New(html.Config{
+    Layout: "layout.html",
+  })).Directory("./templates", ".html")
+
   // use template engine
-  iris.UseTemplate(markdown.New()).Directory("../iris-gitbook", ".md")
+  iris.UseTemplate(markdown.New(markdown.Config{Sanitize:false})).Directory("../iris-gitbook", ".md")
 
   // use recovery middleware
   iris.Use(recovery.New(iris.Logger))
 
+  // change HEADER Server
   iris.Use(&customHeader.CustomHeader{})
 
   iris.Get("/", func(c *iris.Context){
-    c.Render("README.md", struct{}{}, iris.RenderOptions{"gzip":true})
+
+    content := c.TemplateString("README.md", nil, iris.RenderOptions{"gzip":false})
+    c.Render("content.html", struct{Content interface{}}{Content:template.HTML(content)}, iris.RenderOptions{"gzip":false})
 
   })
 
@@ -37,7 +47,8 @@ func main(){
     if file[len(file)-4:] == ".png"{
       c.ServeFile(path, true)
     } else {
-       c.Render(file, struct{}{}, iris.RenderOptions{"gzip":true})
+      content := c.TemplateString(file, nil, iris.RenderOptions{"gzip":false})
+      c.Render("content.html", struct{Content interface{}}{Content:template.HTML(content)}, iris.RenderOptions{"gzip":false})
     }
 
   })
