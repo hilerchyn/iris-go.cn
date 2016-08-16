23837c058fd8832052a37fa35e3397260d138d98
HilerChen <hilerchyn@gmail.com>
2016-08-16 09:28:48 +0800

    implements parse all MarkDown files

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
+import(
+  "github.com/kataras/iris"
+  "github.com/iris-contrib/template/markdown"
+)
+
 func main(){
+
+  iris.UseTemplate(markdown.New()).Directory("../iris-gitbook", ".md")
+
+  iris.Get("/", func(c *iris.Context){
+    c.Render("README.md", struct{}{}, iris.RenderOptions{})
+
+  })
+
+  iris.Get("/:file", func(c *iris.Context){
+    file := c.Param("file")
+
+    c.Render(file, struct{}{}, iris.RenderOptions{})
+  })
+
   println("Hello Iris!")
+
+  iris.Listen(":8080")
 }
