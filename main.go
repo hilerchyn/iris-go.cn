0c5f7a28b39fe0098884f8a72630133b9b2e8951
HilerChen <hilerchyn@gmail.com>
2016-08-16 11:10:38 +0800

    implements recovery

--- main.go
+++ main.go
@@ -2,25 +2,41 @@ package main
 
 import(
   "github.com/kataras/iris"
+  "github.com/kataras/iris/utils"
   "github.com/iris-contrib/template/markdown"
+  "github.com/iris-contrib/middleware/recovery"
 )
 
 func main(){
 
+  // use template engine
   iris.UseTemplate(markdown.New()).Directory("../iris-gitbook", ".md")
 
+  // use recovery middleware
+  iris.Use(recovery.New(iris.Logger))
+
   iris.Get("/", func(c *iris.Context){
-    c.Render("README.md", struct{}{}, iris.RenderOptions{})
+    c.Render("README.md", struct{}{}, iris.RenderOptions{"gzip":true})
 
   })
 
-  iris.Get("/:file", func(c *iris.Context){
-    file := c.Param("file")
+  iris.Get("/:markdown", func(c *iris.Context){
+    file := c.Param("markdown")
 
-    c.Render(file, struct{}{}, iris.RenderOptions{})
+    path := "../iris-gitbook/" + file
+    if !utils.DirectoryExists(path){
+      c.RedirectTo("/")
+      return
+    }
+
+    // serve PNG images
+    if file[len(file)-4:] == ".png"{
+      c.ServeFile(path, true)
+    } else {
+       c.Render(file, struct{}{}, iris.RenderOptions{"gzip":true})
+    }
+
   })
-
-  println("Hello Iris!")
-
+ 
   iris.Listen(":8080")
 }
